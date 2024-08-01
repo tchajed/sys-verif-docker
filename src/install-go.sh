c63502bf4e8be2685cfd660b953ebf820a146905
Tej Chajed <chajed@wisc.edu>
2024-08-01 10:13:26 -0500

    Set some versions explicitly

--- src/install-go.sh
+++ src/install-go.sh
@@ -2,13 +2,16 @@
 
 set -e
 
+GO_VERSION=1.22.5
+
 arch="amd64"
 if [ "$(uname -m)" = "arm64" ]; then
   arch="arm64"
 fi
 
-wget -O go.tar.gz "https://go.dev/dl/go1.22.5.linux-${arch}.tar.gz"
+wget -O go.tar.gz "https://go.dev/dl/go${GO_VERSION}.linux-${arch}.tar.gz"
 sudo tar -C /usr/local -xzf go.tar.gz
+rm go.tar.gz
 
 # shellcheck disable=SC2016
 echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.profile
--- src/install-ocaml.sh
+++ src/install-ocaml.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+OCAML_VERSION=5.2.0
+
 wget 'https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh'
 yes '' | bash install.sh --fresh
 
 # disable sandboxing since the container is enough isolation, and further
 # sandboxing doesn't work in a container anyway
 opam init -y --bare --disable-sandboxing
-opam switch create -y default ocaml-variants.5.2.0+options ocaml-option-flambda
+opam switch create -y default ocaml-variants.${OCAML_VERSION}+options ocaml-option-flambda
 opam clean --logs --all-switches --download-cache --repo-cache --untracked
