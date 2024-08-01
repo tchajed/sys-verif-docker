d1436ed3efbac5db86ccc4279233caf5f59f8dc9
Tej Chajed <chajed@wisc.edu>
2024-08-01 10:31:47 -0500

    Don't try to pin coq

--- src/install-coq.sh
+++ src/install-coq.sh
@@ -2,12 +2,10 @@
 
 set -e
 
-COQ_VERSION=8.19.2
-
 # shellcheck disable=SC2046
 eval $(opam env --switch=default)
 
-opam install -y coq.${COQ_VERSION} vscoq-language-server
+opam install -y coq vscoq-language-server
 # shellcheck disable=SC2016
 echo 'eval $(opam env)' >>~/.profile
 opam clean --logs --all-switches --download-cache --repo-cache --untracked
