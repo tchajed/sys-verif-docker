21e85b8322ca9357f05ded32ee42c36bdfee3577
Tej Chajed <chajed@wisc.edu>
2024-08-07 11:24:57 -0500

    Fix amd64 build issues by removing flambda
    Seems to fix "illegal instruction" errors with both installing OCaml and
    Coq on amd64 (emulated on Apple arm64).

--- src/install-ocaml.sh
+++ src/install-ocaml.sh
@@ -8,10 +8,14 @@ wget 'https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh'
 yes '' | bash install.sh --fresh
 rm install.sh
 
+if [ "$(uname -m)" = "x86_64" ]; then
+  export OPAMJOBS=1
+fi
+
 # disable sandboxing since the container is enough isolation, and further
 # sandboxing doesn't work in a container anyway
 opam init -y --bare --disable-sandboxing
-opam switch create -y default ocaml-variants.${OCAML_VERSION}+options ocaml-option-flambda
+opam switch create -y default ocaml-base-compiler.${OCAML_VERSION}
 
 # shellcheck disable=SC2016
 echo 'eval $(opam env)' >>~/.profile
@@ -19,6 +23,6 @@ echo 'eval $(opam env)' >>~/.profile
 # shellcheck disable=SC2046
 eval $(opam env --switch=default)
 
-opam install -y -j 1 dune
+#opam install -y dune
 
 opam clean --logs --all-switches --download-cache --repo-cache --untracked
